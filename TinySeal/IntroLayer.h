677530bb1f0496d8991127b8cfa7a8738bab8b4d
yuwang88 <yuwang6686@yahoo.co.jp>
2013-11-13 16:30:22 +0900

    Initial Commit

---
+++ TinySeal/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  TinySeal
+//
+//  Created by Yu Wang on 11/13/13.
+//  Copyright Yu Wang 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ TinySeal/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TinySeal
+//
+//  Created by Yu Wang on 11/13/13.
+//  Copyright Yu Wang 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
